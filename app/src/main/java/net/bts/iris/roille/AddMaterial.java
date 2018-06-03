74058b400c87787f6276ff1d1eee0e2392092897
alexiskn <faizeau.alexis@gmail.com>
2018-06-03 20:37:44 +0200

    Add implement in AddMaterial

--- app/src/main/java/net/bts/iris/roille/AddMaterial.java
+++ app/src/main/java/net/bts/iris/roille/AddMaterial.java
@@ -5,10 +5,12 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -16,10 +18,11 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
-public class AddMaterial extends AppCompatActivity implements View.OnClickListener {
+public class AddMaterial extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
 
     private EditText montantMat, nomMat;
-    private Spinner typeMat;
+    private Spinner typeMatSpin;
+    private int typeMat;
     private String idTech;
     private ArrayList<String> typesMat;
     private Button save;
@@ -33,13 +36,15 @@ public class AddMaterial extends AppCompatActivity implements View.OnClickListen
         this.nomMat = (EditText) findViewById(R.id.et_nomMat);
         this.save = (Button) findViewById(R.id.b_enrMat);
         this.save.setOnClickListener(this);
-        this.typeMat = (Spinner) findViewById(R.id.sp_typeMat);
+        this.typeMatSpin = (Spinner) findViewById(R.id.sp_typeMat);
+        this.typeMatSpin.setOnItemSelectedListener(this);
 
         this.typesMat = new ArrayList<String>();
 
         ArrayAdapter spinnerArrayAdapter = new ArrayAdapter(this,
                 android.R.layout.simple_spinner_item, this.typesMat);
-        this.typeMat.setAdapter(spinnerArrayAdapter);
+        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        this.typeMatSpin.setAdapter(spinnerArrayAdapter);
 
 
         final AddMaterial leThis = this;
@@ -72,7 +77,7 @@ public class AddMaterial extends AppCompatActivity implements View.OnClickListen
                     public void run() {
                         int contentType = HttpRequest.post("http://192.168.0.24:3000/materiels/").send(
                                 "montantCaution=" + leThis.montantMat.getText().toString() +
-                                        "&idTypeMateriel=" + (leThis.typeMat.getSelectedItemPosition()+1) +
+                                        "&idTypeMateriel=" + leThis.typeMat +
                                         "&libelle=" + leThis.nomMat.getText().toString()).code();
                         if (contentType == 200) {
                             Intent unIntent = new Intent(leThis, ChoiceAction.class);
@@ -93,5 +98,19 @@ public class AddMaterial extends AppCompatActivity implements View.OnClickListen
 
     public void setTypesMat(ArrayList<String> typesMat) {
         this.typesMat = typesMat;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        // On selecting a spinner item
+        String item = adapterView.getItemAtPosition(i).toString();
+
+        // Showing selected spinner item
+        Toast.makeText(adapterView.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        this.typeMat = 1;
     }
 }
